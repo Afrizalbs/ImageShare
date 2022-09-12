f71e792ca005c87ff706ab896c633f4ecf99a414
Afrizal <afrizalbagas.03@gmail.com>
2022-09-12 10:11:25 +0700

    Config Root React Navigation

--- App.js
+++ App.js
@@ -1,120 +1,21 @@
-import {
-  Image,
-  Platform,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import * as ImagePicker from "expo-image-picker";
-import * as Sharing from "expo-sharing";
-import * as ImageManipulator from "expo-image-manipulator";
-import logo from "./assets/logo.png";
-import { useState } from "react";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet } from "react-native";
+import { PreviewScreen, HomeScreen } from "./Screens";
 
 const App = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  let openImagePicker = async () => {
-    let permissionResult = await ImagePicker.requestCameraPermissionsAsync();
-    if (permissionResult.status === false) {
-      alert("permision to access camera roll is required!");
-      return;
-    }
-
-    let pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-    if (pickerResult.cancelled === true) {
-      return;
-    }
-    setSelectedImage({ localUri: pickerResult.uri });
-  };
-
-  let openShareDialog = async () => {
-    if (Platform.OS === "web") {
-      alert("sharing not available on your platform");
-      return;
-    }
-
-    let imageTmp = await ImageManipulator.manipulateAsync(
-      selectedImage.localUri
-    );
-    await Sharing.shareAsync(imageTmp.uri);
-  };
-
-  if (selectedImage !== null) {
-    return (
-      <View style={styles.container}>
-        <Image
-          style={styles.thumbnail}
-          source={{ uri: selectedImage.localUri }}
-        />
-        <FontAwesome.Button
-          name="share"
-          style={styles.button}
-          onPress={openShareDialog}
-        >
-          Share Photo
-        </FontAwesome.Button>
-      </View>
-    );
-  }
+  const Stack = createNativeStackNavigator();
 
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.logo} />
-
-      <Text style={styles.instructions}>
-        To share a photo from your phone with a friend, just press the button
-        below!
-      </Text>
-
-      <FontAwesome.Button
-        name="folder"
-        style={styles.button}
-        onPress={openImagePicker}
-      >
-        Pick a Photo
-      </FontAwesome.Button>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Preview" component={PreviewScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  logo: {
-    width: 305,
-    height: 159,
-    marginBottom: 10,
-  },
-  instructions: {
-    color: "#888",
-    fontSize: 18,
-    marginHorizontal: 15,
-    marginBottom: 10,
-  },
-  button: {
-    // backgroundColor: "blue",
-    padding: 20,
-    borderRadius: 5,
-  },
-  buttonText: {
-    fontSize: 20,
-    color: "#fff",
-  },
-  thumbnail: {
-    width: 500,
-    height: 500,
-    resizeMode: "contain",
-    marginBottom: 20,
-  },
-});
+const styles = StyleSheet.create({});
 
 export default App;
